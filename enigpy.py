500b639860616f25385067928339845b1e3cc118
Derek Maciel <derekamaciel@gmail.com>
2015-08-10 01:10:54 -0400

    Decoding doesn't work, which means there must be a flaw with the encoding process. I'll have to rethink both

--- enigpy.py
+++ enigpy.py
@@ -31,25 +31,61 @@ class Enigpy:
         self._rotor3 = Rotor(ROTORS[r3]['map'])
         self._rotor2 = Rotor(ROTORS[r2]['map'])
         self._rotor1 = Rotor(ROTORS[r1]['map'])
+        self._reflector = Rotor(REFLECTORS['B']) # A reflector is basically a rotor that never rotates
 
-    def set_rotors(self, rotor3, rotor2, rotor1):
-        pass
+    def set_rotors(self, r3, r2, r1):
+        self._rotor3.set_position(r3)
+        self._rotor2.set_position(r2)
+        self._rotor1.set_position(r1)
 
     def encode(self, c):
-        pass
+        self._rotor1.turn()
+
+        r1 = self._rotor1.encode(c)
+        r2 = self._rotor2.encode(r1)
+        r3 = self._rotor3.encode(r2)
+        ref = self._reflector.encode(r3)
+        r3 = self._rotor3.decode(ref)
+        r2 = self._rotor2.decode(r3)
+        r1 = self._rotor1.decode(r2)
+
+        return r1
 
 
 class Rotor():
-    def __init__(self, name):
+    def __init__(self, map):
         self._position = 0
-        self._map = ROTORS[name]['map']
+        self._map = map
 
     def encode(self, c):
-        return self._map[_letter_to_num(c)]
+        """Encode the given character based on the configuration and position of the current rotor.
+
+        Here is an image of the Enigma rotors:
+        https://en.wikipedia.org/wiki/File:Enigma_rotors_and_spindle_showing_contacts_rachet_and_notch.jpg
+
+        Inputs signals come straight from the input keys to the rotor wheel pins (on the right in the image), but which
+        metal contacts (left) are touching those pins? This depends on the current rotor's position. To figure this out,
+        we add the current rotor position to the input character. For example if the input character is C (and therefore
+        the signal is coming from the C pin) and the rotor is currently showing B in the indicator window, then we know
+        that the C pin is currently touching the D contact. ( (C+B) == (2+1) == 3 == D)
+
+        Once we know what contact the input pin is touching, we check the map to see which output contact is receiving
+        the signal. But which output pin is touching that contact? Simple -- we do the same process but in reverse.
+        """
+        input_contact = _letter_to_num(c) + self._position
+        if input_contact > 25:
+            input_contact -= 25
+
+        output_contact = self._map[input_contact]
+
+        output_pin = _letter_to_num(output_contact) - self._position
+        if output_pin < 0:
+            output_pin += 25
+
+        return _num_to_letter(output_pin)
 
     def decode(self, c):
-        index = self._map.index(c)
-        return _num_to_letter(index)
+        return None
 
     def turn(self):
         if self._position == 25:
